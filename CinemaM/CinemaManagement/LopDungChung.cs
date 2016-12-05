1ff73f8973ecc110cbee3ce0395e217732f1fe41
Hieu <1454050037hieu@ou.edu.vn>
2016-12-05 21:14:45 +0700

    Form, check config, configuration V2

---
+++ CinemaM/CinemaManagement/LopDungChung.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace CinemaManagement
+{
+    public class LopDungChung
+    {
+        public string cnStr;
+        public SqlConnection cn;
+        public int check;
+        public void Connect()
+        {
+            try
+            {
+                if (cn != null && cn.State == ConnectionState.Closed)
+                {
+                    cn.Open();
+                    check = 1;
+                }
+            }
+            catch (SqlException)
+            {
+                MessageBox.Show("Connect Failed!!! Please type your server or database name correctly");
+            }
+            MessageBox.Show("Connect Success!! This application will be restart to save anything you config");
+        }
+        public void Disconnect()
+        {
+            try
+            {
+                if (cn != null && cn.State == ConnectionState.Open)
+                {
+                    cn.Close();
+                    MessageBox.Show("Disconnected");
+                }
+                else check = 0;
+            }
+            catch (SqlException e)
+            {
+                MessageBox.Show(e.Message);
+            }
+        }
+
+    }
+}
