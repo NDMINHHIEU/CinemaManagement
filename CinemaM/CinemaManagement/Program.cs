dca17863d25f95e7cd48883d5935ebf2f4c81250
Van <leduyanh151@gmail.com>
2016-12-10 21:28:49 +0700

    Form Data Entry Clerk (Empty)

--- CinemaM/CinemaManagement/Program.cs
+++ CinemaM/CinemaManagement/Program.cs
@@ -15,7 +15,7 @@ namespace CinemaManagement
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frSplash());
+            Application.Run(new frDataEntry());
         }
     }
 }
---
+++ CinemaM/CinemaManagement/frDataEntry.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace CinemaManagement
+{
+    public partial class frDataEntry : Form
+    {
+        LopDungChung a = new LopDungChung();
+        public frDataEntry()
+        {
+            InitializeComponent();
+        }
+
+        private void DataEntryClerk_Load(object sender, EventArgs e)
+        {
+            a.cnStr = "Server = " + ConfigurationManager.AppSettings["Server"] + "; Database = " + ConfigurationManager.AppSettings["Database"] + ";Integrated Security = true;";
+            a.cn = new System.Data.SqlClient.SqlConnection(a.cnStr);
+
+        }
+
+        private void LoadTable()
+        {
+            
+        }
+
+        private void textBox16_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btPXoa_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+    
+
+  
+    }
+}
