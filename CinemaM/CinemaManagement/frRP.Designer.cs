20cffa97d2720f647dac7b21bdb60a14b4e68db5
Van <nguyenthuyvan0825@gmial.com>
2016-12-23 23:24:37 +0700

    xoa phan du thua

--- CinemaM/CinemaManagement/frRP.Designer.cs
+++ CinemaM/CinemaManagement/frRP.Designer.cs
@@ -47,8 +47,6 @@
             this.button1 = new System.Windows.Forms.Button();
             this.lbThanhTien = new System.Windows.Forms.Label();
             this.label10 = new System.Windows.Forms.Label();
-            this.lbNgayChieu = new System.Windows.Forms.Label();
-            this.label9 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // panel1
@@ -251,28 +249,6 @@
             this.label10.TabIndex = 1;
             this.label10.Text = "label1";
             // 
-            // lbNgayChieu
-            // 
-            this.lbNgayChieu.AutoSize = true;
-            this.lbNgayChieu.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbNgayChieu.Location = new System.Drawing.Point(255, 184);
-            this.lbNgayChieu.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.lbNgayChieu.Name = "lbNgayChieu";
-            this.lbNgayChieu.Size = new System.Drawing.Size(64, 25);
-            this.lbNgayChieu.TabIndex = 1;
-            this.lbNgayChieu.Text = "label1";
-            // 
-            // label9
-            // 
-            this.label9.AutoSize = true;
-            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label9.Location = new System.Drawing.Point(494, 220);
-            this.label9.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(60, 24);
-            this.label9.TabIndex = 1;
-            this.label9.Text = "label1";
-            // 
             // frRP
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
@@ -283,13 +259,11 @@
             this.ClientSize = new System.Drawing.Size(784, 347);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label8);
-            this.Controls.Add(this.label9);
             this.Controls.Add(this.label10);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.lbTenNV);
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.lbNgayChieu);
             this.Controls.Add(this.lbPhongChieu);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.lbDangPhim);
@@ -334,8 +308,6 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label lbThanhTien;
         private System.Windows.Forms.Label label10;
-        private System.Windows.Forms.Label lbNgayChieu;
-        private System.Windows.Forms.Label label9;
 
     }
 }
